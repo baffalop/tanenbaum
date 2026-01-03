61ba0214d32b771fe1eb401f9261642e281378cf
Nikita Gaidakov <ngaidakov@podfather.com>
2026-01-03 17:42:51 +0000

    Make arguments optional. Default to current year, day, part 1.

--- lib/drivers.ml
+++ lib/drivers.ml
@@ -5,16 +5,16 @@ module Cli = struct
 
   module Terms = struct
     let year =
-      let doc = "Run problems from year $(docv)." in
-      Arg.(required & opt (some int) None & info [ "year"; "y" ] ~docv:"YEAR" ~doc)
+      let doc = "Run problems from year $(docv). Uses current year if omitted." in
+      Arg.(value & opt (some int) None & info [ "year"; "y" ] ~docv:"YEAR" ~doc)
 
     let day =
-      let doc = {|Run problem number "day" $(docv).|} in
-      Arg.(required & opt (some int) None & info [ "day"; "d" ] ~docv:"DAY" ~doc)
+      let doc = {|Run problem number "day" $(docv). Uses current day if omitted.|} in
+      Arg.(value & opt (some int) None & info [ "day"; "d" ] ~docv:"DAY" ~doc)
 
     let part =
-      let doc = "Run problem part $(docv)." in
-      Arg.(required & opt (some int) None & info [ "part"; "p" ] ~docv:"PART" ~doc)
+      let doc = "Run problem part $(docv). Defaults to part 1." in
+      Arg.(value & opt int 1 & info [ "part"; "p" ] ~docv:"PART" ~doc)
 
     let auth_token =
       let doc =
@@ -40,11 +40,22 @@ module Cli = struct
       Arg.(value & flag & info [ "submit"; "s" ] ~docv:"SUBMIT" ~doc)
   end
 
-  let run ~year:(year : int) ~day:(day : int) ~part:(part : int)
+  let run ~(year : int option) ~(day : int option) ~(part : int)
       ~example:(example : bool) ~submit:(submit : bool)
       ~token:(auth_token : string option) : unit Cmdliner.Term.ret =
-      let output =
-        let@ (run_mode : Problem_runner.Run_mode.t) =
+      let output : (string, string) result =
+        let@ (year, day) = match year, day with
+          | Some year, Some day -> Ok (year, day)
+          | _, _ ->
+            let time = Unix.localtime @@ Unix.time () in
+            let year = Option.value year ~default:(time.tm_year + 1900) in
+            match day with
+            | Some day -> Ok (year, day)
+            | None ->
+              if time.tm_mon != 11 then Error "Must specify --day if current date is not December"
+              else Ok (year, time.tm_mday)
+        in
+        let@ run_mode : Problem_runner.Run_mode.t =
           match (auth_token, submit, example) with
           | _, true, true ->
               Error {|Cannot use --example and --submit together|}
@@ -58,15 +69,13 @@ module Cli = struct
             in
             Result.ok @@ Problem_runner.Run_mode.Example { input }
           | token, false, _ ->
-              Result.ok @@ Problem_runner.Run_mode.Test_from_puzzle_input {
-                credentials =
-                  Option.map Problem_runner.Credentials.of_auth_token token;
-              }
+            Result.ok @@ Problem_runner.Run_mode.Test_from_puzzle_input {
+              credentials = Option.map Problem_runner.Credentials.of_auth_token token;
+            }
           | Some token, true, _ ->
-              Result.ok @@ Problem_runner.Run_mode.Submit {
-                credentials =
-                  Problem_runner.Credentials.of_auth_token token;
-              }
+            Result.ok @@ Problem_runner.Run_mode.Submit {
+              credentials = Problem_runner.Credentials.of_auth_token token;
+            }
         in
         Problem_runner.(run { year; day; part; run_mode })
       in
